8192c6b200d4727edadb99e006927147b65bbddb
goacoinformasion <goacoinformasion@gmail.com>
2026-09-24 21:07:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -397,7 +397,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:8000/",
         "http://127.0.0.1:5500",# VS Code Live Server
-        ""https://npupgresult.blogspot.com/""  # Production URL
+        "https://npupgresult.blogspot.com/"  # Production URL
     ],
     allow_credentials=True,
     allow_methods=["*"],  # GET, POST, PUT, DELETE sabhi methods allow honge
